112d11855877d0f30815b56b2c2312b8128bfa2f
J Harley <julian.harley@gmail.com>
2025-01-27 22:33:13 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+setup(
+    name="myfibonacci_py",
+    version="0.0.1",
+    author="julz",
+    author_email="axial.nitro0w@icloud.com",
+    description="Calculates a Fibonacci number",
+    long_description="A basic library that \
+      calculates Fibonacci numbers",
+    long_description_content_type="text/markdown",
+    url="https://github.com/julzhk/myfibonacci",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    tests_require=['pytest'],
+)
